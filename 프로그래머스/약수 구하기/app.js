fe1d5682284c69fb984a67decd7e61a4eac24bd7
aquaman122 <swj981128@gmail.com>
2024-09-09 23:17:46 +0900

    add: 약수 구하기

--- 프로그래머스/약수 구하기/app.js
+++ 프로그래머스/약수 구하기/app.js
@@ -5,6 +5,25 @@ let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 solution()
 
-function solution() {
+function solution(left, right) {
+  let answer = 0;
   
+  for (let i = left; i <= right; i++) {
+      const measure = [];
+      // 1. 단순 무식
+      // 2. 주어진 수 절반만
+      // 3. 제곱근
+      for (let j =1; j <= i; j++) {
+          if ( i % j === 0) {
+              measure.push(j);
+          }
+      }
+      
+      if (measure.length % 2 === 0) {
+          answer += i;
+      } else {
+          answer -= i;
+      }
+  }
+  return answer;
 }
